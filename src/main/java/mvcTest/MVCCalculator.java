905b3a8e4410c55d24662225ab944847b1a8d800
S3rjC <serj.cibotarica@gmail.com>
2019-01-21 16:20:04 +0200

    Test commit.

--- src/main/java/mvcTest/MVCCalculator.java
+++ src/main/java/mvcTest/MVCCalculator.java
@@ -2,7 +2,6 @@ package mvcTest;
 
 public class MVCCalculator {
     public static void main(String[] args) {
-        // This is a test comment
         CalculatorView theView = new CalculatorView();
         CalculatorModel theModel = new CalculatorModel();
         CalculatorController theController = new CalculatorController(theView, theModel);
